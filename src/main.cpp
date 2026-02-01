86aa8613ad5e96d5544e25c1b8e85753558ba1e3
WanderROS <wsp1194409532@gmail.com>
2026-02-01 17:00:37 +0800

    es8311

--- src/main.cpp
+++ src/main.cpp
@@ -2,179 +2,181 @@
 #include "Arduino_GFX_Library.h"
 #include "pin_config.h"
 #include "lv_conf.h"
+#include <demos/lv_demos.h>
+#include "TouchDrvCSTXXX.hpp"
 #include <Wire.h>
-#include <SPI.h>
-#include <Arduino.h>
-#include "SensorPCF85063.hpp"
+#include "ESP_I2S.h"
+
+#include "esp_check.h"
+#include "es8311.h"
+#include "canon.h"
 
 #define EXAMPLE_LVGL_TICK_PERIOD_MS 2
 
-static lv_disp_draw_buf_t draw_buf;
-static lv_color_t buf[LCD_WIDTH * LCD_HEIGHT / 10];
+#define MAX_TOUCH_POINTS 5
+int16_t x[MAX_TOUCH_POINTS];
+int16_t y[MAX_TOUCH_POINTS];
+TouchDrvCST92xx CST9217;
+uint8_t touchAddress = 0x5A;
 
-lv_obj_t *label;  // Global label object
-SensorPCF85063 rtc;
-uint32_t lastMillis;
+uint32_t screenWidth;
+uint32_t screenHeight;
+
+static lv_disp_draw_buf_t draw_buf;
 
 Arduino_DataBus *bus = new Arduino_ESP32QSPI(
-  LCD_CS /* CS */, LCD_SCLK /* SCK */, LCD_SDIO0 /* SDIO0 */, LCD_SDIO1 /* SDIO1 */,
-  LCD_SDIO2 /* SDIO2 */, LCD_SDIO3 /* SDIO3 */);
+  LCD_CS, LCD_SCLK, LCD_SDIO0, LCD_SDIO1, LCD_SDIO2, LCD_SDIO3);
 
 Arduino_CO5300 *gfx = new Arduino_CO5300(
   bus, LCD_RESET /* RST */, 0 /* rotation */, LCD_WIDTH /* width */, LCD_HEIGHT /* height */, 6, 0, 0, 0);
 
+I2SClass i2s;
+#define EXAMPLE_SAMPLE_RATE 16000
+#define EXAMPLE_VOICE_VOLUME 90
+#define EXAMPLE_MIC_GAIN (es8311_mic_gain_t)(3)
+
+esp_err_t es8311_codec_init(void) {
+  es8311_handle_t es_handle = es8311_create(0, ES8311_ADDRRES_0);
+  ESP_RETURN_ON_FALSE(es_handle, ESP_FAIL, "ES8311", "create failed");
+
+  const es8311_clock_config_t es_clk = {
+    .mclk_inverted = false,
+    .sclk_inverted = false,
+    .mclk_from_mclk_pin = true,
+    .mclk_frequency = EXAMPLE_SAMPLE_RATE * 256,
+    .sample_frequency = EXAMPLE_SAMPLE_RATE
+  };
+
+  ESP_ERROR_CHECK(es8311_init(es_handle, &es_clk, ES8311_RESOLUTION_16, ES8311_RESOLUTION_16));
+  ESP_ERROR_CHECK(es8311_sample_frequency_config(es_handle, es_clk.mclk_frequency, es_clk.sample_frequency));
+  ESP_ERROR_CHECK(es8311_microphone_config(es_handle, false));
+  ESP_ERROR_CHECK(es8311_voice_volume_set(es_handle, EXAMPLE_VOICE_VOLUME, NULL));
+  ESP_ERROR_CHECK(es8311_microphone_gain_set(es_handle, EXAMPLE_MIC_GAIN));
+  return ESP_OK;
+}
+
+void audio_task(void *param) {
+  i2s.setPins(BCLKPIN, WSPIN, DIPIN, DOPIN, MCLKPIN);
+  if (!i2s.begin(I2S_MODE_STD, EXAMPLE_SAMPLE_RATE, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO, I2S_STD_SLOT_BOTH)) {
+    Serial.println("I2S init failed!");
+    vTaskDelete(NULL);
+  }
+
+  Wire.begin(15, 14);
+  if (es8311_codec_init() != ESP_OK) {
+    Serial.println("ES8311 init failed!");
+    vTaskDelete(NULL);
+  }
+
+  while (1) {
+    i2s.write((uint8_t *)canon_pcm, canon_pcm_len);
+    vTaskDelay(1);
+  }
+}
+
 #if LV_USE_LOG != 0
-/* Serial debugging */
 void my_print(const char *buf) {
   Serial.printf(buf);
   Serial.flush();
 }
 #endif
 
-void example_lvgl_rounder_cb(struct _lv_disp_drv_t *disp_drv, lv_area_t *area)
-{
-    if(area->x1 % 2 !=0)area->x1--;
-    if(area->y1 % 2 !=0)area->y1--;
-    // 变为奇数(如果是偶数就加 1)
-    if(area->x2 %2 ==0)area->x2++;
-    if(area->y2 %2 ==0)area->y2++;
+void example_lvgl_rounder_cb(struct _lv_disp_drv_t *disp_drv, lv_area_t *area) {
+  if(area->x1 % 2 != 0) area->x1--;
+  if(area->y1 % 2 != 0) area->y1--;
+  if(area->x2 % 2 == 0) area->x2++;
+  if(area->y2 % 2 == 0) area->y2++;
 }
 
-
-/* Display flushing */
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
   uint32_t w = (area->x2 - area->x1 + 1);
   uint32_t h = (area->y2 - area->y1 + 1);
-
 #if (LV_COLOR_16_SWAP != 0)
   gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
 #else
   gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
 #endif
-
   lv_disp_flush_ready(disp);
 }
 
 void example_increase_lvgl_tick(void *arg) {
-  /* Tell LVGL how many milliseconds has elapsed */
   lv_tick_inc(EXAMPLE_LVGL_TICK_PERIOD_MS);
 }
 
-static uint8_t count = 0;
-void example_increase_reboot(void *arg) {
-  count++;
-  if (count == 30) {
-    esp_restart();
+void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
+  uint8_t touched = CST9217.getPoint(x, y, CST9217.getSupportTouchPoint());
+  if (touched > 0) {
+    data->state = LV_INDEV_STATE_PR;
+    data->point.x = x[0];
+    data->point.y = y[0];
+    Serial.printf("X:%d Y:%d\n", x[0], y[0]);
+  } else {
+    data->state = LV_INDEV_STATE_REL;
   }
 }
 
 void setup() {
-  Serial.begin(115200); /* prepare for possible serial debug */
-  if (!rtc.begin(Wire, IIC_SDA, IIC_SCL)) {
-    Serial.println("Failed to find PCF8563 - check your wiring!");
-    while (1) {
-      delay(1000);
-    }
-  }
+  Serial.begin(115200);
+  pinMode(PA, OUTPUT);
+  digitalWrite(PA, HIGH);
 
-  uint16_t year = 2024;
-  uint8_t month = 9;
-  uint8_t day = 24;
-  uint8_t hour = 11;
-  uint8_t minute = 9;
-  uint8_t second = 41;
-
-  rtc.setDateTime(year, month, day, hour, minute, second);
-
-  // pinMode(LCD_EN, OUTPUT);
-  // digitalWrite(LCD_EN, HIGH);
+  Wire.begin(IIC_SDA, IIC_SCL);
+  CST9217.begin(Wire, touchAddress, IIC_SDA, IIC_SCL);
+  CST9217.setMaxCoordinates(LCD_WIDTH, LCD_HEIGHT);
+  CST9217.setMirrorXY(true, true);
 
   gfx->begin();
-  gfx->setBrightness(255);
-
-  String LVGL_Arduino = "Hello Arduino! ";
-  LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
-
-  Serial.println(LVGL_Arduino);
-  Serial.println("I am LVGL_Arduino");
+  gfx->setBrightness(200);
+  screenWidth = gfx->width();
+  screenHeight = gfx->height();
 
   lv_init();
 
+  lv_color_t *buf1 = (lv_color_t *)heap_caps_malloc(screenWidth * screenHeight / 4 * sizeof(lv_color_t), MALLOC_CAP_DMA);
+  lv_color_t *buf2 = (lv_color_t *)heap_caps_malloc(screenWidth * screenHeight / 4 * sizeof(lv_color_t), MALLOC_CAP_DMA);
+
 #if LV_USE_LOG != 0
-  lv_log_register_print_cb(my_print); /* register print function for debugging */
+  lv_log_register_print_cb(my_print);
 #endif
 
+  lv_disp_draw_buf_init(&draw_buf, buf1, buf2, screenWidth * screenHeight / 4);
 
-  lv_disp_draw_buf_init(&draw_buf, buf, NULL, LCD_WIDTH * LCD_HEIGHT / 10);
-
-  /*Initialize the display*/
   static lv_disp_drv_t disp_drv;
   lv_disp_drv_init(&disp_drv);
-  /*Change the following line to your display resolution*/
-  disp_drv.hor_res = LCD_WIDTH;
-  disp_drv.ver_res = LCD_HEIGHT;
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
   disp_drv.flush_cb = my_disp_flush;
   disp_drv.rounder_cb = example_lvgl_rounder_cb;
   disp_drv.draw_buf = &draw_buf;
   lv_disp_drv_register(&disp_drv);
 
-  /*Initialize the (dummy) input device driver*/
   static lv_indev_drv_t indev_drv;
   lv_indev_drv_init(&indev_drv);
   indev_drv.type = LV_INDEV_TYPE_POINTER;
+  indev_drv.read_cb = my_touchpad_read;
   lv_indev_drv_register(&indev_drv);
 
-  // lv_obj_t *label = lv_label_create(lv_scr_act());
-  // lv_label_set_text(label, "Hello Ardino and LVGL!");
-  // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_t *label = lv_label_create(lv_scr_act());
+  lv_label_set_text(label, "Hello Arduino and LVGL!");
+  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 
   const esp_timer_create_args_t lvgl_tick_timer_args = {
     .callback = &example_increase_lvgl_tick,
     .name = "lvgl_tick"
   };
 
-  const esp_timer_create_args_t reboot_timer_args = {
-    .callback = &example_increase_reboot,
-    .name = "reboot"
-  };
-
   esp_timer_handle_t lvgl_tick_timer = NULL;
   esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer);
   esp_timer_start_periodic(lvgl_tick_timer, EXAMPLE_LVGL_TICK_PERIOD_MS * 1000);
 
-  label = lv_label_create(lv_scr_act());
-  lv_label_set_text(label, "Initializing...");
-  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+  lv_demo_widgets();  // 你也可以换成其他 demo
+
+  xTaskCreatePinnedToCore(audio_task, "audio_task", 4096, NULL, 1, NULL, 1);
+
+  Serial.println("Setup complete.");
 }
 
 void loop() {
-  lv_timer_handler(); /* let the GUI do its work */
+  lv_timer_handler();
   delay(5);
-
-  if (millis() - lastMillis > 1000) {
-    lastMillis = millis();
-    RTC_DateTime datetime = rtc.getDateTime();
-    Serial.printf(" Year :");
-    Serial.print(datetime.getYear());
-    Serial.printf(" Month:");
-    Serial.print(datetime.getMonth());
-    Serial.printf(" Day :");
-    Serial.print(datetime.getDay());
-    Serial.printf(" Hour:");
-    Serial.print(datetime.getHour());
-    Serial.printf(" Minute:");
-    Serial.print(datetime.getMinute());
-    Serial.printf(" Sec :");
-    Serial.println(datetime.getSecond());
-
-    char buf[32];
-    snprintf(buf, sizeof(buf), "%02d:%02d:%02d\n%02d-%02d-%04d",
-             datetime.getHour(), datetime.getMinute(), datetime.getSecond(),
-             datetime.getDay(), datetime.getMonth(), datetime.getYear());
-
-    // Update label with current time
-    lv_label_set_text(label, buf);
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_40, LV_PART_MAIN);
-  }
-  delay(20);
 }
